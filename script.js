e2b06f2ac2fdc2b85d2c69838e8ab2cf3abf41fd
morris <morristwinomugisha2000@gmail.com>
2024-02-27 23:29:47 +0300

    clean the js file

--- script.js
+++ script.js
@@ -14,12 +14,6 @@ function playRound(playerSelection, computerSelection){
     }
 };
 
-// In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
-// For now, remove the logic that plays exactly five rounds.
-// Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
-// Add a div for displaying results and change all of your console.logs into DOM methods.
-// Display the running score, and announce a winner of the game once one player reaches 5 points.
-// You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
 
 const rock = document.querySelector('#rock-button');
 const paper = document.querySelector("#paper-button");
@@ -63,10 +57,8 @@ function updateScore(result){
 
     if (playerScore === 5) {
         finalScore.textContent = "Finally you win with 5";
-        // console.log("You win")
         } else if (computerScore === 5){
             finalScore.textContent = "The computer wins with 5";
-            // console.log("computer wins")
         } ;
 
 };
