2f74ba0eea11be6e9fcf161cd93c29aa38c04671
morris <morristwinomugisha2000@gmail.com>
2024-02-27 23:22:11 +0300

    Adding DOM functions on the buttons

--- script.js
+++ script.js
@@ -14,36 +14,62 @@ function playRound(playerSelection, computerSelection){
     }
 };
 
-function playGame(){
-    let declareWinner = {};
-    let results = [];
-    let round = 5;
-    while (round >= 1){
-        let playerSelection = prompt("What's your choice");
-        let computerSelection = getComputerChoice().toLowerCase();
-        if (playRound(playerSelection, computerSelection) === "This is a tie"){
-            results.push('t')
-        } else if (playRound(playerSelection, computerSelection) === "Cheers, you have won") {
-            results.push('w')
-        } else {
-            results.push('l')
-        }
-        round = round - 1;
-        }
-    console.log(results)  
-    for (const element of results) {
-        if (declareWinner[element]){
-            declareWinner[element] += 1;
-        }else {
-            declareWinner[element] = 1;
-        }
-    }
-    if (declareWinner.w > declareWinner.l) {
-        console.log("You have won");
-    } else if (declareWinner.w < declareWinner.l){
-        console.log("You have lost");
-    } else {
-        console.log("Its a draw");
-    }
-    }
-playGame();
+// In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+// For now, remove the logic that plays exactly five rounds.
+// Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+// Add a div for displaying results and change all of your console.logs into DOM methods.
+// Display the running score, and announce a winner of the game once one player reaches 5 points.
+// You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+
+const rock = document.querySelector('#rock-button');
+const paper = document.querySelector("#paper-button");
+const scissor = document.querySelector("#scissor-button");
+const result = document.querySelector(".result");
+const score = document.querySelector(".score");
+const finalScore = document.querySelector(".finalScore");
+
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener("click", function(){
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound("rock",computerSelection);
+    result.textContent = roundResult;
+    updateScore(roundResult);
+})
+
+paper.addEventListener("click", function(){
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound("paper",computerSelection);
+    result.textContent = roundResult;
+    updateScore(roundResult);
+})
+
+scissor.addEventListener("click", function(){
+    const computerSelection = getComputerChoice();
+    const roundResult = playRound("scissors",computerSelection);
+    result.textContent = roundResult;
+    updateScore(roundResult);
+})
+
+function updateScore(result){
+    if (result === "Cheers, you have won") {
+        playerScore++;
+    } else if (result === "Opps bad, you have lost") {
+        computerScore++;
+    };
+
+    score.textContent = `player : ${playerScore} - computer: ${computerScore}`;
+
+    if (playerScore === 5) {
+        finalScore.textContent = "Finally you win with 5";
+        // console.log("You win")
+        } else if (computerScore === 5){
+            finalScore.textContent = "The computer wins with 5";
+            // console.log("computer wins")
+        } ;
+
+};
+
+ 
+
